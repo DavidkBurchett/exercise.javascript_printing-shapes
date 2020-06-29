8659ef81947c2a88508c7dd3a32e0d58977b8b86
David_Kyle_Burchett <davidkburchett@gmail.com>
2020-06-29 16:20:39 -0400

    finished line and box

--- assets/js/shapes.js
+++ assets/js/shapes.js
@@ -1,11 +1,24 @@
 function getLine(length) {
     // TODO - write method definition here
+    let output = "";
+    for(let i=0; i<length; i++){
+        output += "*";
+    }
+    return output;
 }
 
 
 
 function getBox(width, height) {
     // TODO - write method definition here
+    let output = "";
+    for(let i=0; i<height; i++){
+        for(let j=0; j<width; j++){
+            output += "*";
+        }
+        output += "\n";
+    }
+    return output
 }
 
 
